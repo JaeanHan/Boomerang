f5c6a300e4d8d0d978624debec994404273b51b6
JaeAnHan <jaean1999@naver.com>
2024-11-05 01:19:06 +0900

    feat: FormatButton 추가
    - bold, italic, underline, strike-though 각각 선택 가능

---
+++ src/components/TextEditor/options/FormatButton.tsx
@@ -0,0 +1,26 @@
+import React, { MutableRefObject } from 'react';
+
+import { BoomerangColors } from '@/utils/colors';
+import { Button } from '@chakra-ui/react';
+
+export const FormatButton: React.FC<{
+  isActive: boolean;
+  onClick: () => void;
+  buttonRef?: MutableRefObject<HTMLButtonElement | null>;
+}> = ({ isActive, onClick, buttonRef, children }) => (
+  <Button
+    onClick={onClick}
+    padding="4px 8px"
+    margin="4px 0"
+    borderRadius="4px"
+    transform={isActive ? 'scale(1.2)' : undefined}
+    backgroundColor={isActive ? BoomerangColors.calmWhite : undefined}
+    variant="unstyled"
+    _hover={{
+      outline: '1px dashed black',
+    }}
+    ref={buttonRef}
+  >
+    {children}
+  </Button>
+);
