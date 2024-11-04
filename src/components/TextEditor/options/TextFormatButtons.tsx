cc90e4667b93267ef3b90345170fecd6bad6b75b
JaeAnHan <jaean1999@naver.com>
2024-11-05 01:22:47 +0900

    feat: TextFormatButtons 추가

---
+++ src/components/TextEditor/options/TextFormatButtons.tsx
@@ -0,0 +1,68 @@
+import { FormatButton } from '@components/TextEditor/options/FormatButton';
+
+import React, { Fragment } from 'react';
+
+import { Text } from '@chakra-ui/react';
+
+const createFormatButtonArray = (
+  isBold: boolean,
+  isItalic: boolean,
+  isUnderline: boolean,
+  isStrikethrough: boolean
+) => {
+  return [
+    {
+      command: 'bold',
+      isActive: isBold,
+      content: <b>B</b>,
+    },
+    {
+      command: 'italic',
+      isActive: isItalic,
+      content: <i>I</i>,
+    },
+    {
+      command: 'underline',
+      isActive: isUnderline,
+      content: <u>U</u>,
+    },
+    {
+      command: 'strikethrough',
+      isActive: isStrikethrough,
+      content: <Text textDecoration="line-through">S</Text>,
+    },
+  ];
+};
+
+export const TextFormatButtons: React.FC<{
+  isBold: boolean;
+  isItalic: boolean;
+  isUnderline: boolean;
+  isStrikethrough: boolean;
+  dispatchTextFormat: (command: string) => void;
+}> = ({
+  isBold,
+  isItalic,
+  isUnderline,
+  isStrikethrough,
+  dispatchTextFormat,
+}) => {
+  return (
+    <Fragment>
+      {createFormatButtonArray(
+        isBold,
+        isItalic,
+        isUnderline,
+        isStrikethrough
+      ).map(({ command, isActive, content }) => (
+        <FormatButton
+          key={command}
+          isActive={isActive}
+          onClick={() => dispatchTextFormat(command)}
+        >
+          {content}
+        </FormatButton>
+      ))}
+    </Fragment>
+  );
+};
