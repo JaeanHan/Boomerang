333bdd8abe017e5992b4a21ef4ba97f62f890807
yunseong0404 <333665@naver.com>
2024-11-09 19:12:09 +0900

    feat: ConsultingManagementSection 레이아웃 구현

---
+++ src/components/ConsultingManagement/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+export const ConsultingManagementSection = () => {
+  return (
+    <Box flex="1" bg="white">
+      ConsultingManagementSection
+    </Box>
+  );
+};
