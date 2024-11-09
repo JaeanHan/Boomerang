0e791e4c4ebb4683607d963ac198e48ec3c406ab
yunseong0404 <333665@naver.com>
2024-11-10 01:52:38 +0900

    feat: 객체 배열으로 변경

--- src/components/Consulting/ExpertSection/MostAnswersExpertSection.tsx
+++ src/components/Consulting/ExpertSection/MostAnswersExpertSection.tsx
@@ -2,17 +2,53 @@ import { MentorCard } from '@/components/Consulting/MentorCard';
 import { MentorSelectionHeader } from '@/components/Consulting/MentorSelectionHeader';
 import { Box, Flex } from '@chakra-ui/react';
 
+const MostAnswersExpert = [
+  {
+    name: '김땡땡',
+    matchingCount: 33,
+  },
+  {
+    name: '김땡땡',
+    matchingCount: 33,
+  },
+  {
+    name: '김땡땡',
+    matchingCount: 33,
+  },
+];
+
+const MostAnswersMentor = [
+  {
+    name: '김땡땡',
+    matchingCount: 33,
+  },
+  {
+    name: '김땡땡',
+    matchingCount: 33,
+  },
+  {
+    name: '김땡땡',
+    matchingCount: 33,
+  },
+];
+
 export const MostAnswersExpertSection = () => {
   return (
-    <Box h="669px" bg="#D9D9D9" borderRadius={22} mt="45px" p="26px 31px">
+    <Box h="669px" bg="#F3F3F3" borderRadius={22} mt="25px" p="26px 31px">
       <Box>
         <MentorSelectionHeader
           title="가장 답변을 많이 단 위원회⚡"
           subtitle="전세사기 위원회 경력 3년 이상"
         />
         <Flex gap="63px" mt="25px">
-          {Array.from({ length: 2 }).map((_, index) => (
-            <MentorCard key={index} w="399px" h="189px" />
+          {MostAnswersExpert.map((mentor) => (
+            <MentorCard
+              key={mentor.name}
+              name={mentor.name}
+              matchingCount={mentor.matchingCount}
+              w="399px"
+              h="189px"
+            />
           ))}
         </Flex>
       </Box>
@@ -23,8 +59,14 @@ export const MostAnswersExpertSection = () => {
           subtitle="보험없이 소송으로 해결한 경험자"
         />
         <Flex gap="63px" mt="25px">
-          {Array.from({ length: 2 }).map((_, index) => (
-            <MentorCard key={index} w="399px" h="189px" />
+          {MostAnswersMentor.map((mentor) => (
+            <MentorCard
+              key={mentor.name}
+              name={mentor.name}
+              matchingCount={mentor.matchingCount}
+              w="399px"
+              h="189px"
+            />
           ))}
         </Flex>
       </Box>
