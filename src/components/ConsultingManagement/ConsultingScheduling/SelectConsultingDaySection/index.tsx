84b6285549301052b5d2b2c5a54e6ceed9ca9887
JaeAnHan <jaean1999@naver.com>
2024-11-12 17:47:12 +0900

    feat: 타임 피커 추가 (스타일 변경필요)

--- src/components/ConsultingManagement/ConsultingScheduling/SelectConsultingDaySection/index.tsx
+++ src/components/ConsultingManagement/ConsultingScheduling/SelectConsultingDaySection/index.tsx
@@ -2,31 +2,35 @@ import { useState } from 'react';
 import Calendar from 'react-calendar';
 
 import { ConsultingItemTitle } from '@/components/ConsultingManagement/ConsultingItemTitle';
-import { Box, Flex, VStack } from '@chakra-ui/react';
+import { BoomerangColors } from '@/utils/colors';
+import { Box, Button, Flex, VStack } from '@chakra-ui/react';
 import pointer from '@images/pointer.svg';
 
 import './index.css';
+
+const times = Array.from({ length: 15 }, (_, i) => {
+  const hour = (9 + i).toString().padStart(2, '0');
+  return `${hour}:00`;
+});
 
 export const SelectConsultingDaySection = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedTime, setSelectedTime] = useState('');
 
-  const times = [
-    '09:00',
-    '10:00',
-    '11:00',
-    '12:00',
-    '13:00',
-    '14:00',
-    '15:00',
-    '16:00',
-    '17:00',
-    '18:00',
-  ];
+  const reservedTimes = {
+    '2024-11-12': ['10:00', '12:00'],
+    '2024-11-13': ['09:00', '13:00', '15:00'],
+  };
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
     setSelectedTime('');
+  };
+
+  const getReservedTimes = () => {
+    if (!selectedDate) return [];
+    const dateString = selectedDate.toISOString().split('T')[0];
+    return reservedTimes[dateString] || [];
   };
 
   return (
@@ -36,35 +40,48 @@ export const SelectConsultingDaySection = () => {
         icon={pointer}
       />
       <VStack justifyContent="center">
-        <Flex w="870px" h="394px" borderRadius="17px" bg="#F4F4F4">
+        <Flex w="870px" h="394px" borderRadius="17px">
           <Calendar
             onChange={handleDateChange}
             value={selectedDate}
-            locale={'ko'}
+            locale="ko"
             calendarType="gregory"
             view="month"
             prev2Label={null}
             next2Label={null}
             showNeighboringMonth={false}
           />
-          {selectedDate && (
-            <div>
-              <h3>시간 선택</h3>
-              <select
-                value={selectedTime}
-                onChange={(e) => setSelectedTime(e.target.value)}
-              >
-                <option value="" disabled>
-                  시간을 선택하세요
-                </option>
-                {times.map((time) => (
-                  <option key={time} value={time} disabled={false}>
-                    {time}
-                  </option>
-                ))}
-              </select>
-            </div>
-          )}
+          <Box
+            borderTop={`2px solid ${BoomerangColors.deepBlue}`}
+            borderBottom={`2px solid ${BoomerangColors.deepBlue}`}
+          >
+            <Flex
+              maxW={300}
+              wrap="wrap"
+              justifyContent="center"
+              alignItems={'center'}
+              gap={2}
+              h={'100%'}
+              p={'5px 0'}
+            >
+              {times.map((time) => (
+                <Button
+                  key={time}
+                  onClick={() => setSelectedTime(time)}
+                  isDisabled={getReservedTimes().includes(time)}
+                  // border={`1px solid ${BoomerangColors.blue}`}
+                  border={'none'}
+                  variant={selectedTime === time ? 'solid' : 'outline'}
+                  width="70px"
+                  height="40px"
+                  fontSize={'1rem'}
+                  fontWeight={500}
+                >
+                  {time}
+                </Button>
+              ))}
+            </Flex>
+          </Box>
         </Flex>
       </VStack>
     </Box>
