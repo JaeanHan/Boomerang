d0062ab47685c7262a2a4c091dfd51321b2a9bb9
JaeAnHan <jaean1999@naver.com>
2024-11-14 03:04:41 +0900

    fix: Comment 수정한 댓글로 표시 안되던 부분 수정

--- src/components/ForumPost/Comment.tsx
+++ src/components/ForumPost/Comment.tsx
@@ -22,16 +22,18 @@ export const Comment: React.FC<CommentProps> = ({ comment, removeComment }) => {
   const { user } = useUserContext();
 
   const handleEditConfirm = () => {
-    updateComment(comment.id, editedText).catch((err) => {
-      setEditedText(comment.text);
-      toast({
-        title: '댓글 수정에 실패했습니다.',
-        description: err.response?.data?.message || '',
-        status: 'error',
-        duration: 2000,
-        isClosable: true,
+    updateComment(comment.id, editedText)
+      .then(() => setIsEditing(false))
+      .catch((err) => {
+        setEditedText(comment.text);
+        toast({
+          title: '댓글 수정에 실패했습니다.',
+          description: err.response?.data?.message || '',
+          status: 'error',
+          duration: 2000,
+          isClosable: true,
+        });
       });
-    });
   };
 
   const handleDelete = async () => {
@@ -65,7 +67,7 @@ export const Comment: React.FC<CommentProps> = ({ comment, removeComment }) => {
       ) : (
         <Fragment>
           <Text fontSize="xl" color="gray.700" whiteSpace={'preserve-breaks'}>
-            {comment.text}
+            {editedText}
           </Text>
           <Text fontSize="sm" color="gray.500" mt={2}>
             {comment.writer_name} {formatDate(comment.last_modified_at)}
