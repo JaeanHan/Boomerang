c377a5fe75b555fdb0c6d65c278bbec75a348e46
JaeAnHan <jaean1999@naver.com>
2024-11-14 03:15:43 +0900

    feat: AutoSizingTextarea 기본 프롭 setContent를 optional로 수정

--- src/components/AutoSizingTextarea/index.tsx
+++ src/components/AutoSizingTextarea/index.tsx
@@ -3,19 +3,20 @@ import { useCallback } from 'react';
 import { Textarea, TextareaProps } from '@chakra-ui/react';
 
 interface AutoSizingTextareaProps extends TextareaProps {
-  setContent: (newContent: string) => void;
+  setContent?: (newContent: string) => void;
 }
-export const AutoSizingTextarea: React.FC<
-  AutoSizingTextareaProps & {
-    setContent: (newContent: string) => void;
-  }
-> = ({ setContent, ...chakraStyles }) => {
+export const AutoSizingTextarea: React.FC<AutoSizingTextareaProps> = ({
+  setContent,
+  ...chakraStyles
+}) => {
   const onChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
       const element = e.target;
       element.style.height = 'auto';
       element.style.height = `${element.scrollHeight}px`;
-      setContent(element.value);
+      if (setContent) {
+        setContent(element.value);
+      }
     },
     [setContent]
   );
