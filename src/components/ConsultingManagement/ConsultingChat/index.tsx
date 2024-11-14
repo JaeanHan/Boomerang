f5e1b2e36ebd25be303be2c49e26654d0e732265
JaeAnHan <jaean1999@naver.com>
2024-11-15 04:05:46 +0900

    fix: useInfiniteConsultRecords 타입 명시

--- src/components/ConsultingManagement/ConsultingChat/index.tsx
+++ src/components/ConsultingManagement/ConsultingChat/index.tsx
@@ -1,16 +1,22 @@
+import { useNavigate, useParams } from 'react-router-dom';
+
 import { ChatSection } from '@/components/ConsultingManagement/ConsultingChat/ChatSection';
-import { ConsultingInfoSection } from '@/components/ConsultingManagement/ConsultingChat/ConsultingInfoSection';
 import { ConsultingManagementHeader } from '@/components/ConsultingManagement/ConsultingManagementHeader';
+import { ROUTER_PATH } from '@/routerPath';
 import { Box, VStack } from '@chakra-ui/react';
 
 export const ConsultingChat = () => {
-  // const chatId = useParams();
+  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  if (!id) {
+    navigate(ROUTER_PATH.CONSULTING_START);
+    return null;
+  }
 
   return (
     <Box flex="1" bg="white">
       <ConsultingManagementHeader category="상담 채팅" />
       <VStack mt="55px" spacing="52px">
-        <ConsultingInfoSection />
         <ChatSection />
       </VStack>
     </Box>
