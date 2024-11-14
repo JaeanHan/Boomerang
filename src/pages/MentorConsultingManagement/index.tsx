8529f42b13aded7eaf5a25e42ff5f822444424e6
yunseong0404 <333665@naver.com>
2024-11-14 21:14:48 +0900

    feat: MentorConsultingManagement 구현

---
+++ src/pages/MentorConsultingManagement/index.tsx
@@ -0,0 +1,21 @@
+import { ConsultingOutlet } from '@components/ConsultingManagement/ConsultingOutlet';
+
+import { Sidebar } from '@/components/Sidebar';
+import { SidebarOpenBtn } from '@/components/Sidebar/SidebarOpenBtn';
+import { BasicLayout } from '@/components/commons/BasicLayout';
+import { SidebarProvider } from '@/pages/ConsultingManagement/SidebarContext';
+import { Flex } from '@chakra-ui/react';
+
+export const MentorConsultingManagement = () => {
+  return (
+    <BasicLayout maxW="100%">
+      <Flex>
+        <SidebarProvider>
+          <SidebarOpenBtn />
+          <Sidebar />
+          <ConsultingOutlet />
+        </SidebarProvider>
+      </Flex>
+    </BasicLayout>
+  );
+};
