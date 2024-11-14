660cf3cb901c4e5436d464b0a1238667e97d6489
JaeAnHan <jaean1999@naver.com>
2024-11-14 18:54:37 +0900

    feat: 멘토 상담신청 페이지 스타일 수정

--- src/components/ConsultingManagement/ConsultingScheduling/index.tsx
+++ src/components/ConsultingManagement/ConsultingScheduling/index.tsx
@@ -4,16 +4,22 @@ import { ConsultingManagementHeader } from '@/components/ConsultingManagement/Co
 import { ConsultingInformationInputSection } from '@/components/ConsultingManagement/ConsultingScheduling/ConsultingInformationInputSection';
 import { SelectConsultingDaySection } from '@/components/ConsultingManagement/ConsultingScheduling/SelectConsultingDaySection';
 import { MentorDetailCard } from '@/components/ConsultingManagement/MentorDetailCard';
+import { useSidebar } from '@/pages/ConsultingManagement/SidebarContext';
 import { Box, VStack } from '@chakra-ui/react';
 
 export const ConsultingScheduling = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [selectedTime, setSelectedTime] = useState<string>('');
-
+  const { isSidebarOpen } = useSidebar();
   return (
     <Box flex="1" bg="white">
       <ConsultingManagementHeader category="멘토 상담예약하기" />
-      <VStack mt="55px" pb="69px" spacing="52px">
+      <VStack
+        mt="55px"
+        pb="69px"
+        spacing="52px"
+        ml={isSidebarOpen ? '60px' : 0}
+      >
         <MentorDetailCard />
         <SelectConsultingDaySection
           selectedDate={selectedDate}
