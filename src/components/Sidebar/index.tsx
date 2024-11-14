8945b00c433d18a1ba82de141c924d2c6142a6bb
yunseong0404 <333665@naver.com>
2024-11-14 22:00:38 +0900

    fix: 사이드바 멘토 카테고리 추가

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,9 +3,12 @@ import { sidebarWidth } from '@components/Sidebar/constants';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { MentorCalendar } from '@/components/Sidebar/sidebarIcons/MentorCalendar';
 import { MentorCharge } from '@/components/Sidebar/sidebarIcons/MentorCharge';
+import { MentorCheck } from '@/components/Sidebar/sidebarIcons/MentorCheck';
 import { MentorEmergency } from '@/components/Sidebar/sidebarIcons/MentorEmergency';
 import { MentorMessage } from '@/components/Sidebar/sidebarIcons/MentorMessage';
+import { MentorPaper } from '@/components/Sidebar/sidebarIcons/MentorPaper';
 import { MentorPeople } from '@/components/Sidebar/sidebarIcons/MentorPeople';
 import { MentorSearch } from '@/components/Sidebar/sidebarIcons/MentorSearch';
 import { useSidebar } from '@/pages/ConsultingManagement/SidebarContext';
@@ -29,8 +32,10 @@ export const Sidebar: React.FC = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('');
   const navigate = useNavigate();
+  //TODO: 임시
+  const userType = 'MENTOR';
 
-  const sidebarCategories = [
+  const menteeSidebarCategories = [
     {
       title: '멘토 선택하기',
       icon: MentorPeople,
@@ -57,6 +62,42 @@ export const Sidebar: React.FC = () => {
       path: '',
     },
   ];
+
+  const mentorSidebarCategories = [
+    {
+      title: '상담 일자 등록하기',
+      icon: MentorCalendar,
+      path: ROUTER_PATH.MENTOR_DATE_REGISTRATION,
+    },
+    {
+      title: '신청 내역 확인하기',
+      icon: MentorCheck,
+      path: ROUTER_PATH.MENTOR_CONFIRM_REQUEST,
+    },
+    {
+      title: '상담 예정 내용 확인하기',
+      icon: MentorPaper,
+      path: ROUTER_PATH.MENTOR_SCHEDULED,
+    },
+    {
+      title: '과거 상담 내역 조회하기',
+      icon: MentorSearch,
+      path: ROUTER_PATH.MENTOR_CONSULTING_HISTORY,
+    },
+    {
+      title: '불편 사항 신고',
+      icon: MentorEmergency,
+      path: '',
+    },
+    {
+      title: '포인트 충전하기',
+      icon: MentorCharge,
+      path: '',
+    },
+  ];
+
+  const sidebarCategories =
+    userType === 'MENTOR' ? mentorSidebarCategories : menteeSidebarCategories;
 
   const handleButtonClick = (category: string, path: string) => {
     setSelectedCategory(category);
