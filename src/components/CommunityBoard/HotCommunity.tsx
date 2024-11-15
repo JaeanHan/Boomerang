77e5af63d7e138430db6256f82e7471ecd34ec8c
JaeAnHan <jaean1999@naver.com>
2024-11-15 15:15:04 +0900

    fix: 리버트 수정 테스트

--- src/components/CommunityBoard/HotCommunity.tsx
+++ src/components/CommunityBoard/HotCommunity.tsx
@@ -27,7 +27,7 @@ export const HotCommunity: React.FC<HotCommunityProps> = ({ boardType }) => {
         fontWeight="bold"
         color="#363636"
       >
-        {hotTopics.map((topic) => (
+        {hotTopics.map((topic, idx) => (
           <HotTopic key={topic.postId} title={topic.title} id={topic.postId} />
         ))}
       </Flex>
