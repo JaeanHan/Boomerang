f7e086fe3c169e790bb13b592cf45092672a47e8
yunseong0404 <333665@naver.com>
2024-11-15 15:09:25 +0900

    fix: ConsultingInfoSection 수정

--- src/components/ConsultingManagement/ConsultingChat/ConsultingInfoSection/index.tsx
+++ src/components/ConsultingManagement/ConsultingChat/ConsultingInfoSection/index.tsx
@@ -1,17 +1,19 @@
-import { ConsultingInfoBox } from '@/components/ConsultingManagement/ConsultingInfoBox';
+import {
+  ConsultingInfoBox,
+  ConsultingInfoItem,
+} from '@/components/ConsultingManagement/ConsultingInfoBox';
 import { Box, Text } from '@chakra-ui/react';
 
-const ConsultingInfo = [
+const ConsultingInfo: ConsultingInfoItem[] = [
   {
-    title: '상담 일정',
-    content: '24/10/22 오후 3시~ 오후 4시',
+    consultation_date_time: '24/10/22 오후 3시~ 오후 4시',
+    mentor_nick_name: '김땡땡',
+    content:
+      '주택 전세사기를 당했어요... 주택 전세사기를 당했어요...주택 전세사기를 당했어요...주택 전세사기를 당했어요...',
   },
   {
-    title: '멘토명',
-    content: '김땡땡',
-  },
-  {
-    title: '신청 내용',
+    consultation_date_time: '24/10/22 오후 3시~ 오후 4시',
+    mentor_nick_name: '김땡땡',
     content:
       '주택 전세사기를 당했어요... 주택 전세사기를 당했어요...주택 전세사기를 당했어요...주택 전세사기를 당했어요...',
   },
@@ -23,7 +25,9 @@ export const ConsultingInfoSection = () => {
       <Text fontSize="18px" fontWeight="bold" color="#979797" mb="17px">
         신청된 상담 내역
       </Text>
-      <ConsultingInfoBox infoList={ConsultingInfo} />
+      {ConsultingInfo.map((infoList) => (
+        <ConsultingInfoBox infoList={infoList} />
+      ))}
       <Text
         fontSize="15px"
         fontWeight="bold"
