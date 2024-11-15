a13fdfbbccb4940bd5e366d992f35f9d11b80e9a
JaeAnHan <jaean1999@naver.com>
2024-11-15 11:46:48 +0900

    feat: 메인 페이지 배너 재사용 가능도록 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { HeroSection } from '@components/HeroSection';
 import { MainSection } from '@components/MainSection';
 import { BasicLayout } from '@components/commons/BasicLayout';
 
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Spacer, VStack } from '@chakra-ui/react';
 
@@ -12,7 +12,15 @@ export const Home: React.FC = () => {
   return (
     <BasicLayout maxW={1024}>
       <VStack spacing={10} align="stretch">
-        <HeroSection h={300} />
+        <HeroSection
+          h={300}
+          upperText={
+            <Fragment>
+              <b>전세사기 대응 가이드라인</b> 저희가 드릴게요!
+            </Fragment>
+          }
+          whiteBoxText={'전세 사기를 당했어요! 어떻게 하죠?😢'}
+        />
         <MainSection h={492} />
         <ConsultationSection h={470} />
         <CommunitySection h={700} />
